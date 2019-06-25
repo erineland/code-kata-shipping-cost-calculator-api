7bce3a54a15b4cc55a9ac7aeed6ea94c9a12046d
Erin Eland <erin@erineland.com>
2019-06-25 16:27:26 +0100

    First test for simple free inner state package now passing

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,9 +4,37 @@ var express = require('express');
 // const r = require('ramda')
 const router = express.Router()
 
+// Same state uses the base rate on the weight;
+// Different state shipments have a fixed 20% cost increase on top of the previous value (rate on weight);
+// Packages can have distinct weight units, either pounds or kilograms.
+// Weight needs to be validated on pounds, thus requiring conversion from kg;
+// Weight rate is tiered:
+// Shipments up to 2 pounds are free;
+// When pounds are > 2, each pound will be 1 dollar;
+// When pounds are > 20, each pound will be 2 dollars;
+// Examples:
+// 1 pound shipment will be free;
+// 3 pound shipment will be 3 dollars;
+// 8 pound shipment will be 8 dollars;
+// 25 pound shipment will be 50 dollars;
+
 router.post('/request-shipment', function(req, res, next) {
 	// TODO implement based on README
-	res.status(200).send({ shipment: { cost: 'Dummy Shipment' } })
+	// res.status(200).send({ shipment: { cost: 'Dummy Shipment' } })
+
+	console.log(`req object is: ${req}`);
+
+	const shipmentRequest = req.body;
+
+	// Iterate packages
+	shipmentRequest.packages.forEach(parcel => {
+		// Check unit of measurement for weight
+		if (parcel.unit === 'LB') {
+			if (parcel.weight < 2) {
+				res.status(200).send({ shipment: { cost: 0 }});
+			}
+		}
+	});
 });
 
 module.exports = router;
